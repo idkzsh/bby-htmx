7311ef23b0406207972561a07a85380f92a6f746
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-17 11:54:39 -0800

    feat: - added header component and styled it - added switch/toggle for theme - changes to global.css/tailwind.config - changed alpine to cdn as the packages have issues with persist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 import htmx from "astro-htmx";
-
 import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), tailwind(), htmx()],
+  integrations: [tailwind(), htmx()],
   output: "server",
   adapter: node({
     mode: "standalone"

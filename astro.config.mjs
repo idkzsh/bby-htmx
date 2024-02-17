e3feee5a54028b5cef863c115e4eaf61094d1832
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-16 20:56:33 -0800

    fixed node config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import htmx from "astro-htmx";
+
 import node from "@astrojs/node";
 
 // https://astro.build/config

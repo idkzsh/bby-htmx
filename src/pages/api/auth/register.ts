3de6cb663c1bbe6ad3e7e8116e9dddc63beaed20
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-18 13:05:35 -0800

    created supabase auth

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+
+  if (!email || !password) {
+    console.log("error is here")
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
+  return redirect("/signin");
+};

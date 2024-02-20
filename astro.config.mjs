eaea83333a0fde3504b02022cf987591a11052f4
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-19 23:30:28 -0800

    added vercel ssr dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import htmx from "astro-htmx";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), htmx()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });

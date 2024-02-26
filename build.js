79acc486357ac642616080420b46d30e9f4d392c
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-26 13:59:44 -0800

    literally anything

--- build.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-const dir = 'node_modules/pdfjs-dist/build/pdf.mjs';
-const content = fs.readFileSync(dir, { encoding: 'utf-8' });
-fs.writeFileSync(dir, content.replace('"./pdf.worker.mjs";', `__dirname + "/pdf.worker.mjs";`))
